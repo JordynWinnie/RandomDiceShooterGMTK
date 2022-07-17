8ae289b78e9eb829d061ad50a8e90985338d962e
Nathaniel <semblancewarden@Gmail.com>
2022-07-18 01:22:17 +0800

    Bug Fixes/Enemy Object Pooling

---
+++ Assets/DamageFlash/DamageFlash.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.DamageFlash
+{
+    public class DamageFlash : MonoBehaviour
+    {
+        [SerializeField] private Material flashMat;
+        [SerializeField] private MeshRenderer meshRenderer;
+        [SerializeField] private float blinkDuration = 0.075f;
+
+        private List<Material> materials = new List<Material>();
+        private List<Material> flashMats = new List<Material>();
+
+        private void Start()
+        {
+            for (int i = 0; i < meshRenderer.materials.Length; i++)
+            {
+                materials.Add(meshRenderer.materials[i]);
+                flashMats.Add(flashMat);
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (materials.Count > 0)
+            {
+                meshRenderer.materials = materials.ToArray();
+            }
+        }
+
+        public void Flash()
+        {
+            meshRenderer.materials = flashMats.ToArray();
+
+            StartCoroutine(FadeDelay());
+        }
+
+        private IEnumerator FadeDelay()
+        {
+            yield return new WaitForSeconds(blinkDuration);
+            for (int i = 0; i < meshRenderer.materials.Length; i++)
+            {
+                meshRenderer.materials = materials.ToArray();
+            }
+        }
+
+        private void OnValidate()
+        {
+            if (meshRenderer == null)
+            {
+                gameObject.TryGetComponent(out meshRenderer);
+            }
+        }
+    }
+}
